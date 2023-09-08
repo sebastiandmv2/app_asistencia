7c986165014505ba275fb39d75635aadf7513547
namiva18 <silva.nicolasn@gmail.com>
2023-09-07 23:09:24 -0300

    actualizacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private animationCtrl: AnimationController) {}
 
+  ionViewDidEnter() {
+    const button = document.getElementById('login-button');
+
+    // Verifica si el botón existe en la vista
+    if (button) {
+      const animation = this.animationCtrl.create()
+        .addElement(button)
+        .duration(1000) // Duración de la animación en milisegundos
+        .fromTo('transform', 'scale(-5)', 'scale(1)'); //  aumento de tamaño
+
+      animation.play();
+    }
+  }
 }
